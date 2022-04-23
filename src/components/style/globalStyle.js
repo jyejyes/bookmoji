19db4605225e569721172802b4ba962cfeddc2a7
brie <jh100m1@gmail.com>
2022-04-24 00:38:47 +0900

    :lipstick: :: #1 웹폰트 적용

--- src/components/style/globalStyle.js
+++ src/components/style/globalStyle.js
@@ -3,7 +3,22 @@ import { createGlobalStyle } from "styled-components";
 import "./reset.css";
 
 export const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'LeferiBaseType-RegularA';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/LeferiBaseType-RegularA.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'LeferiBaseType-BoldA';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/LeferiBaseType-BoldA.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+ 
+
     html{
+        font-family: 'LeferiBaseType-RegularA';
         font-size:62.5%;
         width:100%;
         height:100%;
