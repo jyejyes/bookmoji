46683ce835435c1b047bcaceaa0f43dca65fb927
brie <jh100m1@gmail.com>
2022-04-24 01:25:49 +0900

    :lipstick: :: #1 theme 파일 추가

---
+++ src/components/style/theme.js
@@ -0,0 +1,19 @@
+import { css } from "styled-components";
+
+export const flexCenter = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const color = {
+  Main: "#f53a57",
+  black: "#151515",
+  white: "#FFFFFF",
+  light_gray: "#f5f5f5",
+  light_gray2: "#eeeeee",
+  dark_gray: "#555555",
+  dark_gray2: "#333333",
+  medium_gray: "#dddddd",
+  medium_gray2: "#8c8c8c",
+};
