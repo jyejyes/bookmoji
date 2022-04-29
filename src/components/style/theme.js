8756bebd16855eed0af48040d4ca1099c8ace956
brie <jh100m1@gmail.com>
2022-04-29 21:10:16 +0900

    :lipstick: :: #1 style 파일 수정
    auth 버튼, 인풋창 등 공통 스타일 추가

--- src/components/style/theme.js
+++ src/components/style/theme.js
@@ -7,7 +7,7 @@ export const flexCenter = css`
 `;
 
 export const color = {
-  Main: "#f53a57",
+  Main: "#D8837A",
   black: "#151515",
   white: "#FFFFFF",
   light_gray: "#f5f5f5",
@@ -22,11 +22,14 @@ export const blackButton = css`
   font-family: "LeferiBaseType-RegularA";
   margin-right: 1rem;
   border-radius: 2.5rem;
-  padding: 1rem 1.2rem;
+  padding: 0.8rem 1rem;
   color: ${color.white};
   background: ${color.black};
   &:hover {
     background: ${color.dark_gray2};
+  }
+  &:focus {
+    outline: none;
   }
 `;
 
@@ -34,10 +37,55 @@ export const whiteButton = css`
   font-family: "LeferiBaseType-RegularA";
   margin-right: 1rem;
   border-radius: 3rem;
-  padding: 1rem 1.2rem;
+  padding: 0.8rem 1rem;
   background: ${color.white};
   color: ${color.black};
   &:hover {
     background: ${color.light_gray};
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const mainColorButton = css`
+  font-family: "LeferiBaseType-RegularA";
+  margin-right: 1rem;
+  border-radius: 3rem;
+  padding: 0.8rem 1rem;
+  background: ${color.Main};
+  color: ${color.white};
+`;
+
+export const authLabel = css`
+  font-size: 1.3rem;
+  font-weight: 800;
+  margin: 1rem;
+`;
+
+export const authInput = css`
+  ${whiteButton}
+  border: 1px solid ${color.medium_gray2};
+  width: 100%;
+  font-size: 1.5rem;
+  padding: 1.3rem 2rem;
+  box-sizing: border-box;
+  margin-bottom: 1rem;
+  &:hover {
+    background: ${color.white};
+  }
+  &:focus {
+    border: 1px solid ${color.Main};
+  }
+`;
+
+export const authButton = css`
+  ${mainColorButton}
+  border: 1px solid ${color.Main};
+  padding: 1.3rem 2rem;
+  font-size: 1.5rem;
+  width: 100%;
+  &:focus {
+    outline: none;
   }
 `;
