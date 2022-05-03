1722f1e5acb41ef45af85fd27f89a79823788375
brie <jh100m1@gmail.com>
2022-05-03 17:09:12 +0900

    :art: :: #7 모달창 형태 제작
    설정페이지 이슈 넘버 줘도 되나 모르겠지만 설정페이지 위해 처음 만든거니까..

---
+++ src/components/common/Modal.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import DeleteUserModal from "../Users/ModalContent/DeleteUserModal";
+import LogoutModal from "../Users/ModalContent/LogoutModal";
+import { color } from "../style/theme";
+
+// 함수 props 로 넘기기
+const Modal = ({ handleOpenModal, whatBtn }) => {
+  // 모달 오픈시 배경 화면 스크롤 방지
+  document.body.style.overflow = "hidden";
+
+  return (
+    <ModalWrapper>
+      <BackModal onClick={handleOpenModal} />
+      <div className="modal">
+        {whatBtn === "logout" && (
+          <LogoutModal handleOpenModal={handleOpenModal} />
+        )}
+        {whatBtn === "deleteuser" && <DeleteUserModal />}
+      </div>
+    </ModalWrapper>
+  );
+};
+
+export default Modal;
+
+const ModalWrapper = styled.div`
+  z-index: 200;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+
+  .modal {
+    background: ${color.white};
+    z-index: 500;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 1rem;
+    padding: 3rem;
+    box-sizing: border-box;
+  }
+`;
+
+// 배경
+const BackModal = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
