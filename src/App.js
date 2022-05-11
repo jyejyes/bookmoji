be2e55ff11c78ea0431634a8fd8930ffcd8a95bc
brie <jh100m1@gmail.com>
2022-05-11 23:17:17 +0900

    :sparkles: :: #15 HOC 적용
    근데 userPage에만 적용한 상태임..

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Access from "./components/common/Access";
 import { GlobalStyle } from "./components/style/globalStyle";
 import AuthPage from "./page/Auth/AuthPage";
+import CollectPage from "./page/CollectPage";
 import MainPage from "./page/MainPage";
+import SearchPage from "./page/SearchPage";
+import UserPage from "./page/UserPage";
 
 function App() {
+  const AccessUsersPage = Access(UserPage, true);
   return (
     <>
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/auth/:auth" element={<AuthPage />} />
+        <Route path="/search/:bookName" element={<SearchPage />} />
+        <Route path="/users" element={<AccessUsersPage />} />
+        <Route path="/collect" element={<CollectPage />} />
       </Routes>
     </>
   );
