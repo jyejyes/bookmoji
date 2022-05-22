f812c83e34bbf64aa5a84c8c2b4b46cac87f867e
brie <jh100m1@gmail.com>
2022-05-22 19:39:11 +0900

    :lipstick: :: #19 메인페이지 소개 컴포넌트 추가

---
+++ src/components/Main/Introduce.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { flexCenter } from "../style/theme";
+import MainDisscription from "./MainDiscription";
+import MainSubject from "./MainSubject";
+
+const Introduce = () => {
+  return (
+    <Wrapper>
+      <MainSubject content="후기에 감정만을 담아보세요" />
+      <MainDisscription content="매번 달라지던 별점이 아닌" />
+      <MainDisscription content="온전한 당신의 마음을 후기에 담아보세요 !" />
+      <img src="https://velog.velcdn.com/images/jh100m1/post/21355425-26ff-4964-a69a-d8446af9fcb9/image.png" />
+    </Wrapper>
+  );
+};
+
+export default Introduce;
+
+const Wrapper = styled.div`
+  width: 100%;
+  margin: 4rem 0;
+  ${flexCenter}
+  flex-direction: column;
+
+  img {
+    width: 100%;
+    margin: 3rem 0;
+  }
+`;
