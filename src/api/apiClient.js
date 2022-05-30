96aa6e30376905f7cc3720b18c0ab8507be9d56f
brie <p_jihye97@naver.com>
2022-05-30 16:33:20 +0900

    :bug: :: #14 axios interceptors 사용하여 header 에 토큰 바로넣기

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 export const apiClient = axios.create({
   baseURL: "https://bookmoji.site/",
+});
+
+apiClient.interceptors.request.use((config) => ({
+  ...config,
   headers: {
     "X-ACCESS-TOKEN": localStorage.getItem("jwt"),
   },
-});
+}));
