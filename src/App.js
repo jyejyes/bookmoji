b3885a6896f745f74b9a4e2e13ee159a831be26c
brie <p_jihye97@naver.com>
2022-06-02 13:06:13 +0900

    :art: :: #22 모바일 검색창 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Access from "./components/common/Access";
+import Footer from "./components/Footer/Footer";
 import { GlobalStyle } from "./components/style/globalStyle";
 import AuthPage from "./page/Auth/AuthPage";
 import CollectPage from "./page/CollectPage";
 import MainPage from "./page/MainPage";
+import MobileSearch from "./page/MobileSearch";
 import SearchPage from "./page/SearchPage";
 import UserPage from "./page/UserPage";
 
 function App() {
   const AccessUsersPage = Access(UserPage, true);
+
   return (
     <>
       <GlobalStyle />
@@ -17,8 +20,9 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/auth/:auth" element={<AuthPage />} />
         <Route path="/search/:bookName" element={<SearchPage />} />
+        <Route path="/m/search/" element={<MobileSearch />} />
         <Route path="/users" element={<AccessUsersPage />} />
-        <Route path="/collect" element={<CollectPage />} />
+        <Route path="/collect" element={<AccessCollectPage />} />
       </Routes>
     </>
   );
