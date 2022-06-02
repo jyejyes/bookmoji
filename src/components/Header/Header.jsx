c8fe085219fba857fe5c6e60aaa6d587c4fc9176
brie <p_jihye97@naver.com>
2022-06-02 13:55:23 +0900

    :iphone: :: #22 헤더 반응형 색상 변경, 검색창 등 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
 import { ReactComponent as Logo } from "../../svg/logo.svg";
-import { ReactComponent as Search } from "../../svg/ic-search.svg";
+import SearchBar from "../common/SearchBar";
+
 import {
   blackButton,
   color,
+  device,
   flexCenter,
   mainColorButton,
   whiteButton,
@@ -13,7 +15,6 @@ import {
 
 const Header = () => {
   const navigate = useNavigate();
-  const [search, setSearch] = useState("");
 
   const onClickNavigate = (e) => {
     if (e.target.name === "logo") navigate("/");
@@ -22,73 +23,60 @@ const Header = () => {
     if (e.target.name === "collect") navigate("/collect");
   };
 
-  const onInputSearch = (e) => {
-    setSearch(e.target.value);
-  };
-
-  const onSubmitSearch = (e) => {
-    if (e.key === "Enter") {
-      navigate(`/search/${search}`);
-    }
-  };
   // 로그인 유무 임시판별
   const [isLogin, setIsLogin] = useState(localStorage.getItem("jwt"));
   return (
-    <HeaderWrapper>
-      <div>
-        <LogoStyle onClick={() => navigate("/")} name="logo" />
-        <div className="search-auth">
-          <div className="search">
-            <SearchStyle />
-            <input
-              type="search"
-              placeholder="책 이름, 저자, 출판사로 검색해보세요."
-              onChange={onInputSearch}
-              onKeyPress={onSubmitSearch}
-              value={search}
-            />
+    <>
+      <HeaderWrapper>
+        <div>
+          <LogoStyle onClick={() => navigate("/")} name="logo" />
+          <div className="search-auth">
+            <div className="search">
+              <SearchBar />
+            </div>
+
+            {!isLogin ? (
+              // 로그인 안했을 때
+              <>
+                <button
+                  className="register"
+                  onClick={onClickNavigate}
+                  value="register"
+                  name="auth"
+                >
+                  회원가입
+                </button>
+                <button
+                  className="login"
+                  onClick={onClickNavigate}
+                  value="login"
+                  name="auth"
+                >
+                  로그인
+                </button>
+              </>
+            ) : (
+              // 로그인 했을 때
+              <>
+                <button
+                  className="move-record"
+                  name="collect"
+                  onClick={onClickNavigate}
+                >
+                  모아보기
+                </button>
+                <img
+                  onClick={onClickNavigate}
+                  className="user"
+                  name="users"
+                  src="https://an2-glx.amz.wtchn.net/assets/default/user/photo_file_name_small-ab0a7f6a92a282859192ba17dd4822023e22273e168c2daf05795e5171e66446.jpg"
+                />
+              </>
+            )}
           </div>
-          {!isLogin ? (
-            // 로그인 안했을 때
-            <>
-              <button
-                className="register"
-                onClick={onClickNavigate}
-                value="register"
-                name="auth"
-              >
-                회원가입
-              </button>
-              <button
-                className="login"
-                onClick={onClickNavigate}
-                value="login"
-                name="auth"
-              >
-                로그인
-              </button>
-            </>
-          ) : (
-            // 로그인 했을 때
-            <>
-              <button
-                className="move-record"
-                name="collect"
-                onClick={onClickNavigate}
-              >
-                모아보기
-              </button>
-              <img
-                onClick={onClickNavigate}
-                className="user"
-                name="users"
-                src="https://an2-glx.amz.wtchn.net/assets/default/user/photo_file_name_small-ab0a7f6a92a282859192ba17dd4822023e22273e168c2daf05795e5171e66446.jpg"
-              />
-            </>
-          )}
         </div>
-      </div>
-    </HeaderWrapper>
+      </HeaderWrapper>
+    </>
   );
 };
 
@@ -102,6 +90,10 @@ const HeaderWrapper = styled.header`
   position: sticky;
   top: 0;
   z-index: 100;
+  @media ${device.mobile} {
+    background: ${color.Main};
+    box-shadow: 0px 0px 0px 0px #efefef;
+  }
 
   & > div {
     width: 92.5%;
@@ -110,30 +102,38 @@ const HeaderWrapper = styled.header`
   }
   .search-auth {
     ${flexCenter}
-    input {
-      width: 29rem;
-      border: none;
-      border-radius: 0.5rem;
-      background: ${color.light_gray2};
-      box-sizing: border-box;
-      padding: 1rem 1rem 1rem 3.5rem;
-      margin-right: 1rem;
-      font-size: 1.4rem;
-      color: ${color.dark_gray2};
-      &:focus {
-        outline: none;
+
+    .search {
+      @media ${device.mobile} {
+        display: none;
       }
     }
+
     .register {
       ${mainColorButton}
+      @media ${device.mobile} {
+        font-size: 1.3rem;
+        ${whiteButton}
+      }
     }
     .login {
       ${whiteButton}
+      @media ${device.mobile} {
+        font-size: 1.3rem;
+        ${mainColorButton}
+        &:hover {
+          background: ${color.Main};
+        }
+      }
     }
     // 로그인 시
     .move-record {
       ${whiteButton}
       color: ${color.dark_gray};
+
+      @media ${device.mobile} {
+        display: none;
+      }
     }
 
     .user {
@@ -142,6 +142,10 @@ const HeaderWrapper = styled.header`
       width: 3.3rem;
       height: 3.3rem;
       cursor: pointer;
+
+      @media ${device.mobile} {
+        display: none;
+      }
     }
   }
   .search {
@@ -152,11 +156,10 @@ const HeaderWrapper = styled.header`
 const LogoStyle = styled(Logo)`
   cursor: pointer;
   fill: ${color.Main};
-`;
-
-const SearchStyle = styled(Search)`
-  position: absolute;
-  top: 50%;
-  transform: translateY(-55%);
-  left: 1.2rem;
+  width: 6rem;
+  @media ${device.mobile} {
+    font-size: 1.2rem;
+    width: 5.5rem;
+    fill: white;
+  }
 `;
