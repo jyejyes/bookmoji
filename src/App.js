3fd3d5407b0cb3fa9cdf68c95e6fb389b2f7d9c6
brie <p_jihye97@naver.com>
2022-06-04 00:05:39 +0900

    :wrench: :: #1 redux 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { Route, Routes } from "react-router-dom";
+import { createStore } from "redux";
 import Access from "./components/common/Access";
-import Footer from "./components/Footer/Footer";
 import { GlobalStyle } from "./components/style/globalStyle";
 import AuthPage from "./page/Auth/AuthPage";
 import CollectPage from "./page/CollectPage";
@@ -9,21 +10,27 @@ import MainPage from "./page/MainPage";
 import MobileSearch from "./page/MobileSearch";
 import SearchPage from "./page/SearchPage";
 import UserPage from "./page/UserPage";
+import rootReducer from "./store";
 
 function App() {
   const AccessUsersPage = Access(UserPage, true);
   const AccessCollectPage = Access(CollectPage, true);
+
+  const store = createStore(rootReducer);
+
   return (
     <>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/auth/:auth" element={<AuthPage />} />
-        <Route path="/search/:bookName" element={<SearchPage />} />
-        <Route path="/m/search/" element={<MobileSearch />} />
-        <Route path="/users" element={<AccessUsersPage />} />
-        <Route path="/collect" element={<AccessCollectPage />} />
-      </Routes>
+      <Provider store={store}>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/auth/:auth" element={<AuthPage />} />
+          <Route path="/search/:bookName" element={<SearchPage />} />
+          <Route path="/m/search/" element={<MobileSearch />} />
+          <Route path="/users" element={<AccessUsersPage />} />
+          <Route path="/collect" element={<AccessCollectPage />} />
+        </Routes>
+      </Provider>
     </>
   );
 }
