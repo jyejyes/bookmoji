bd0643a2bf28c2d1719b1db4586315b06d2f64d6
brie <p_jihye97@naver.com>
2022-06-06 22:32:38 +0900

    :sparkles: :: #24 페이지 이동시 스크롤 위치 상단으로 가는 기능 추가

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./ScrollTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     {/* <React.StrictMode> */}
+    <ScrollToTop />
     <App />
     {/* </React.StrictMode> */}
   </BrowserRouter>
