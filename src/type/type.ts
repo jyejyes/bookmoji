f15a731308eb791dabc82798209b4e43b4213608
brie <p_jihye97@naver.com>
2023-02-11 19:25:38 +0900

    :memo: 타입스크립트 타입 파일

---
+++ src/type/type.ts
@@ -0,0 +1,92 @@
+//api
+export interface ApiResult<T> {
+  code: number;
+  isSuccess: boolean;
+  message: string;
+  result: T;
+}
+
+//로그인 타입
+export interface LoginFormData {
+  email: string;
+  password: string;
+}
+//회원가입 타입
+export interface RegisterFormData extends LoginFormData {
+  pwCheck?: string;
+  nickname: string;
+}
+
+//책 기본 타입
+export interface BookProps {
+  author?: string;
+  emoji: string;
+  introduction?: string;
+  isbn?: string;
+  publisher?: string;
+  releaseYear?: string;
+  reviewIdx: number;
+  thumbnailUrl: string;
+  title: string;
+}
+
+//내 리뷰 책 타임
+export interface MyReviewProps extends BookProps {
+  reviewTime: string;
+  text: string;
+}
+
+// 다른책 리뷰 타입
+export interface GetOtherReviewsProps {
+  result: OtherReviewProps[];
+}
+export interface OtherReviewProps extends BookProps {
+  hasLiked: number;
+  text: string;
+}
+
+// 카카오 책 검색 API props
+export interface GetKakaoBooksProps {
+  documents: KakaoBook[];
+  meta: {
+    is_end: boolean;
+    pageable_count: number;
+    total_count: number;
+  };
+}
+
+export interface KakaoBook {
+  authors: string[];
+  contents: string;
+  datetime: string;
+  isbn: string;
+  price: number;
+  publisher: string;
+  sale_price: number;
+  status: string;
+  thumbnail: string;
+  title: string;
+  translators: string[];
+  url: string;
+}
+
+//유저 페이지
+export interface UserNicknameFormData {
+  nickname: string;
+}
+export interface UserPassworkFormData {
+  currentPw: string;
+  password: string;
+}
+
+//월별 차트 데이터 타입
+export interface MonthChartData {
+  month: number;
+  monthlyCount: number;
+}
+
+//이모지별 차트 데이터 타입
+export interface EmojiChartData {
+  emoji: string;
+  emojiPercentage: string;
+}
