c59a4cd0b1e46c6c49e8c30d656d83f1692af8e1
brie <p_jihye97@naver.com>
2023-02-11 19:15:16 +0900

    :recycle: api 관련 파일 ts 로 변경

--- src/api/bookSearch.ts
+++ src/api/bookSearch.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-const Kakao = axios.create({
+interface BookSearchProps {
+  query: string;
+  page: number;
+  size: number;
+}
+
+export const Kakao = axios.create({
   baseURL: "https://dapi.kakao.com",
   headers: {
     Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_REST_API}`,
@@ -8,6 +14,6 @@ const Kakao = axios.create({
 });
 
 //search book api
-export const bookSearch = (params) => {
+export const bookSearch = (params: BookSearchProps) => {
   return Kakao.get("/v3/search/book", { params });
 };
