14119b9500af7c0d902830e75d6e463514ca3c14
brie <p_jihye97@naver.com>
2023-02-11 19:29:19 +0900

    :memo: ts 변경파일, yarn 파일

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./ScrollTop";
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    {/* <React.StrictMode> */}
-    <ScrollToTop />
-    <App />
-    {/* </React.StrictMode> */}
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <ScrollToTop />
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>
   </BrowserRouter>
 );
