8fe1b3b8925af23eb1b15866022f1b2686884a2e
brie <p_jihye97@naver.com>
2023-02-11 16:45:41 +0900

    :memo: react-query querykey 파일 추가

---
+++ src/constant/queryKeys.ts
@@ -0,0 +1,9 @@
+export const queryKeys = {
+  OTHER_REVIEW_KEY: "otherReview", //다른 사람 리뷰 GET
+  KAKAO_BOOKS_KEY: "kakaoBooks", //카카오 책 검색 GET
+  KAKAO_BOOK_KEY: "kakaoBook", //카카오 책 검색 (1개만) GET
+  USER_NICKNAME_KEY: "userNickname", //회원 정보(닉네임) 불러오기 GET
+  REVIEW_EMOJI_KEY: "reviewEmoji", // 리뷰 이모지 통계 조회 GET
+  REVIEW_MONTH_KEY: "reviewMonth", // 리뷰 월별 통계 조회 GET
+  USER_BOOK_REVIEW_KEY: "userBookReview", //유저 책 리뷰 조회 GET
+};
